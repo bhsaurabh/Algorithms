8a8cb5ac86ad427008b45ab5a412d2a602f46fb3
bhsaurabh <srbhatia10@gmail.com>
2014-02-28 13:00:01 +0530

    Commited by script on Fri Feb 28 13:00:01 IST 2014

--- collinear/Point.java
+++ collinear/Point.java
@@ -15,7 +15,7 @@ import java.util.Comparator;
 public class Point implements Comparable<Point> {
 
     // compare points by slope
-    public final Comparator<Point> SLOPE_ORDER;       // YOUR DEFINITION HERE
+    public final Comparator<Point> SLOPE_ORDER = new BySlope();
 
     private final int x;                              // x coordinate
     private final int y;                              // y coordinate
@@ -41,19 +41,48 @@ public class Point implements Comparable<Point> {
 
     // slope between this point and that point
     public double slopeTo(Point that) {
-        /* YOUR CODE HERE */
+        // m = (y2 - y1) / (x2 - x1)
+        int dy = that.getY() - this.getY(); // using accessors
+        int dx = that.getX() - this.getX();
+        if (dy == 0 && dx == 0) return Double.MIN_VALUE;
+        if (dx == 0)    return Double.MAX_VALUE;
+        if (dy == 0)    return 0.0;
+        return (1.0 * dy)/dx;
     }
 
     // is this point lexicographically smaller than that one?
     // comparing y-coordinates and breaking ties by x-coordinates
     public int compareTo(Point that) {
-        /* YOUR CODE HERE */
+        int y2 = that.getY(), y1 = this.getY(), x2 = that.getX(), x1 = this.getX();
+        if (y1 > y2)    return +1;
+        if (y1 < y2)    return -1;
+        if (x1 > x2)    return +1;
+        if (x1 < x2)    return -1;
+        return 0;
     }
 
     // return string representation of this point
     public String toString() {
         /* DO NOT MODIFY */
         return "(" + x + ", " + y + ")";
+    }
+
+    // accessors
+    public int getX() { return this.x; }
+    public int getY() { return this.y; }
+
+    // mutators
+    public void setX(int x) { this.x = x; }
+    public void setY(int y) { this.y = y; }
+
+    // Comparator class for slope based comparisons
+    private class BySlope implements Comparator<Point>
+    {
+        public int compare(Point v, Point w)
+        {
+            // compare based on slope made from invoking point
+            return this.slopeTo(v) - this.slopeTo(w);
+        }
     }
 
     // unit test
