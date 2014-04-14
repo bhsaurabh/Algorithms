fedab392ca706d1bbe4b6b70b61a7b4f17f6b337
Saurabh Bhatia <saurabh.bhatia@me.com>
2014-04-14 20:56:31 +0530

    Negative cycle check added

--- BellmanFord.java
+++ BellmanFord.java
@@ -4,10 +4,12 @@ public class BellmanFord {
 
     private DirectedEdge[] edgeTo;
     private double[] distTo;
+    private boolean hasNegativeCycle;
 
     public BellmanFord(EdgeWeightedGraph G, int s) {
         this.distTo = new double[G.V()];
         this.edgeTo = new DirectedEdge[G.V()];
+        this.hasNegativeCycle = false;
 
         // initialise distances
         for (int v = 0; v < G.V(); v++)
@@ -15,18 +17,29 @@ public class BellmanFord {
         distTo[s] = 0.0;
 
         // repeat V times...relax all edges
-        for (int v = 0; v < G.V(); v++)
+        for (int v = 0; v < G.V(); v++) {
+            boolean temp = false;
             for (DirectedEdge e : G.edges())
-                relax(e);
+                temp |= relax(e);
+            if (temp && v == G.V()-1)   hasNegativeCycle = true;
+        }
     }
 
     /* Helper: Relax a directed edge */
-    private void relax(DirectedEdge e) {
+    private boolean relax(DirectedEdge e) {
         // get vertices
         int v = e.from(), w = e.to();
         if (distTo[w] > distTo[v] + e.weight()) {
             distTo[w] = distTo[v] + e.weight();
             edgeTo[w] = e;
+            return true;
         }
+        return false;
+    }
+
+    /* Negative Cycles: If there is an update to distTo made in last iteration
+     * There exists a negative cycle. Use edgeTo[v] to trace cycle */
+    public boolean hasNegativeCycle() {
+        return hasNegativeCycle;
     }
 }
