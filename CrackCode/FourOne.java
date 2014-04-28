561cf164028a77d5e826733a817d127f143938da
def <srbhatia10@gmail.com>
2014-04-28 21:02:53 +0530

    Practice codes

--- CrackCode/FourOne.java
+++ CrackCode/FourOne.java
@@ -27,11 +27,61 @@ public class FourOne {
      *
      * @return the height of the tree
      */
-    private int getHeight(Node root) {
+    private static int getHeight(Node root) {
         if (root == null) {
             return 0;   // base case
         }
 
         return Math.max(getHeight(root.left), getHeight(root.right)) + 1;
+    }
+
+    /**
+     * Finds height of the node. This is an optimised version of the code.
+     *
+     * @param root is the root of the tree in question
+     *
+     * @return the height of the tree rooted at root
+     * Returns -1 if the tree is not balanced at root
+     */
+    private static int checkHeight(Node root) {
+        if (root == null) {
+            return 0;   // base case
+        }
+
+        int leftHeight = checkHeight(root.left);
+        if (leftHeight == -1) {
+            // not balanced at root.left
+            return -1;
+        }
+
+        int rightHeight = checkHeight(root.right);
+        if (rightHeight == -1) {
+            // not balanced at root.right
+            return -1;
+        }
+
+        /* Check if the current node is balanced */
+        int heightDiff = leftHeight - rightHeight;
+        if (Math.abs(heightDiff) > 1) {
+            // current node is not balanced
+            return -1;
+        } else {
+            return Math.max(leftHeight, rightHeight) + 1;
+        }
+    }
+
+    /** 
+     * Optimised check if tree is balanced of not
+     *
+     * @param root is the root of the tree
+     *
+     * @return true if balanced, false otherwise
+     */
+    public static boolean isBalanced2(Node root) {
+        if (checkHeight(root) == -1) {
+            return false;
+        } else {
+            return true;
+        }
     }
 }
